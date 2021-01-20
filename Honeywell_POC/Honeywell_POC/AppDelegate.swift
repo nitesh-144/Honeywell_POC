f8f824d325ae0553a6b3602ce92b0247124aaf4f
Nitesh Vyas <niteshvyas144@gmail.com>
2021-01-20 09:54:03 +0530

    - commit 1.) Project created 2.) Files setup - Removing unnecessary files and some code cleanup

---
+++ Honeywell_POC/Honeywell_POC/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Honeywell_POC
+//
+//  Created by Deepika on 20/01/21.
+//  Copyright © 2021 freedom. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+
---
+++ Honeywell_POC/Honeywell_POC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Honeywell_POC
+//
+//  Created by Deepika on 20/01/21.
+//  Copyright © 2021 freedom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
